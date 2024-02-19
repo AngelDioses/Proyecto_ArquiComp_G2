55b48abd4b1e3924f7ba52259f5054a1a01ac9ea
Angel Dioses <angel.dioses@unmsm.edu.pe>
2024-02-19 18:32:55 -0500

    config.c

--- config.c
+++ config.c
@@ -5,13 +5,11 @@
 #include "xlcd.h"
 
 
-void Configurar_Puertos(void) {
-    
+void Configurar_Puertos(void) {    
     TRISA = 0xFFFF; // Configura todo el puerto A como entrada
     TRISB = 0xFFF0; // Configura RB0 a RB3 como entrada (Dip Switches), el resto como salida
-    
-    int LeerDipSwitch(void) {
-    return (PORTB & 0x0F); // Leer los primeros 4 bits de PORTB donde están los Dip Switches
+}
 
-    }
+int LeerDipSwitch(void) {
+    return (PORTB & 0x0F); // Leer los primeros 4 bits de PORTB donde están los Dip Switches
 }
